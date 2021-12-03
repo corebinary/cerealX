6087eaac51bf45d76313f64f1ead74e3456c5fc6
CoreBinary <johndoe@example.com>
2021-12-03 00:58:21 -0500

    WORKING

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,17 @@ void pixhawk_USB();
 void RFD900_Pixhawk();
 void RFD900_Jetson();
 void RFD900_OPENHD_Pixhawk();
+void RFD900_OPENHD_Jetson();
 
 SoftwareSerial softSerial(22, 23); // RX, TX
 SoftwareSerial softSerial1(19, 21); // RX, TX
 
-int GPIO_STATE = 0;
-int GPIO36 = 36;
-int GPIO39 = 39;
-int GPIO34 = 34;
-
-int incomingByte = 0; // for incoming serial data
-
 void setup() {
-  softSerial.begin(57600); //PIXHAWK  - Tx=GPIO23 Rx=GPIO22 
-  softSerial1.begin(57600); //OPENHD  - Tx=GPIO2 Rx=GPIO4
   Serial.begin(57600);  //JETSON - USB
   Serial2.begin(57600); //RFD900 - Tx=GPIO17 Rx=GPIO16 
+  softSerial.begin(57600); //PIXHAWK  - Tx=GPIO23 Rx=GPIO22 
+  softSerial1.begin(57600); //OPENHD  - Tx=GPIO21 Rx=GPIO19
+    
   pinMode(14,OUTPUT); //PIXhawk active
   pinMode(12,OUTPUT); //Jetson Active
   pinMode(36, INPUT); // RFD GPIO 3 
@@ -30,10 +25,9 @@ void setup() {
 }
 
 void loop() {
-  //RFD900_Pixhawk();
-  //pixhawk_USB();
-  GPIO_STATE = 0;
   //GET THE GPIO SETTINGS
+  int GPIO_STATE = 0;
+    
   if (digitalRead(36)){
     GPIO_STATE = GPIO_STATE + 4;
   }
@@ -48,18 +42,19 @@ void loop() {
 
   switch (GPIO_STATE){
     case 5: //ESP Mode
-      diag(); //Test
+      diag(); 
       digitalWrite(14,HIGH);
       digitalWrite(12,HIGH);
       break;
     case 6: //Jetson Mode
       RFD900_Jetson();
+      //RFD900_OPENHD_Jetson();
       digitalWrite(14,LOW);
       digitalWrite(12,HIGH);
       break;
     default: //Pixhawk mode
-      //RFD900_Pixhawk();
-      RFD900_OPENHD_Pixhawk();
+      RFD900_Pixhawk();
+      //RFD900_OPENHD_Pixhawk();
       digitalWrite(14,HIGH);
       digitalWrite(12,LOW);
       break;
@@ -67,11 +62,11 @@ void loop() {
 }
 
 void diag(){
-  incomingByte = 0;
+  int incomingByte = 0;
   if (Serial2.available()) {     
-    Serial2.write("***DIAGNOSTOC MODE: Press 1 to reboot ESP32:***\n"); 
+    Serial2.println("***DIAGNOSTOC MODE: Press 1 to reboot ESP32:***\n"); 
      incomingByte = Serial2.read();  
-     if (incomingByte==49){Serial2.write("REBOOTING in 3 seconds...");delay(3000);ESP.restart();}
+     if (incomingByte==49){Serial2.println("REBOOTING in 3 seconds...\n");delay(3000);ESP.restart();}
   }
 }
 
@@ -116,5 +111,19 @@ void RFD900_OPENHD_Pixhawk(){
 
   if (Serial2.available()) {    
     softSerial.write(Serial2.read());   
+  }
+}
+
+void RFD900_OPENHD_Jetson(){
+  if (Serial.available()) {  
+    byte SERIAL_DATA = Serial.read();
+    Serial2.write(SERIAL_DATA); 
+    if (softSerial1.available()) {
+        softSerial1.write(SERIAL_DATA);
+      } 
+  }
+
+  if (Serial2.available()) {    
+    Serial.write(Serial2.read());   
   }
 }
